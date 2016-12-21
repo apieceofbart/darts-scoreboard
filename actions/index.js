86ef4786d1de16a0a8116c39a9a541533361a65c
apieceofbart <bartek.kozera@gmail.com>
2016-12-21 17:58:19 +0100

    adding reducers

--- actions/index.js
+++ actions/index.js
@@ -1,26 +1,36 @@
 export const ADD_PLAYER = 'ADD_PLAYER';
 export const REMOVE_PLAYER = 'REMOVE_PLAYER';
 export const EDIT_PLAYER = 'EDIT_PLAYER';
+export const CHANGE_SCORE = 'CHANGE_SCORE';
+
+let nextPlayerId = 0;
 
 export function addPlayer(name) {
-	return {
-		type: ADD_PLAYER,
-		name
-	}
+  return {
+    type: ADD_PLAYER,
+    id: nextPlayerId++
+    name,
+  }
 };
 
 export function removePlayer(id) {
-	return {
-		type: REMOVE_PLAYER,
-		id
-	}
+  return {
+    type: REMOVE_PLAYER,
+    id
+  }
 };
 
-
 export function editPlayer(id, name) {
-	return {
-		type: EDIT_PLAYER,
-		id,
-		name
-	}
+  return {
+    type: EDIT_PLAYER,
+    id,
+    name
+  }
+};
+
+export function changeScore(id, score) {
+  return {
+    type: CHANGE_SCORE,
+    score
+  }
 };
---
+++ reducers/index.js
@@ -0,0 +1,39 @@
+import { combineReducers } from 'redux'
+import { ADD_PLAYER, REMOVE_PLAYER, EDIT_PLAYER, CHANGE_SCORE } from './actions'
+
+function players(state = [], action) {
+  switch (action.type) {
+    case ADD_PLAYER:
+      return [
+        ...state,
+        {
+          name: action.name,
+          id: action.id
+        }
+      ]
+    case REMOVE_PLAYER:
+      return state.filter(player => player.id !== action.id);
+    case EDIT_PLAYER:
+      return state.map(player => {
+        if (player.id === action.id) {
+          return { ...player, name: action.name }
+        }
+        return player
+      })
+    case CHANGE_SCORE:
+      return state.map(player => {
+        if (player.id === action.id) {
+          return { ...player, score: action.score }
+        }
+        return player;
+      })
+    default:
+      return state
+  }
+}
+
+const dartsApp = combineReducers({
+  players
+})
+
+export default dartsApp
